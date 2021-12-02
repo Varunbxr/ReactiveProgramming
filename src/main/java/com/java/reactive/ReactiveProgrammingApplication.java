17690b65250d42a8ceb725b003622731f20f7183
Varun Prakash <varunbxr92@gmail.com>
2021-12-02 20:17:29 +0530

    Intitial Commit

---
+++ src/main/java/com/java/reactive/ReactiveProgrammingApplication.java
@@ -0,0 +1,14 @@
+package com.java.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(ReactiveProgrammingApplication.class, args);
+	}
+
+}
